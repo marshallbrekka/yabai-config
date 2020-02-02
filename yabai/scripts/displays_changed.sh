ba06e25119b1f16e6d7ff431ed2e43d135e7ffda
marshallbrekka <marshallb@fair.com>
2020-02-02 14:24:50 -0800

    Make slighty more reliable

--- yabai/scripts/displays_changed.sh
+++ yabai/scripts/displays_changed.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
+sleep 1
+$HOME/.config/yabai/scripts/ensure_spaces.sh
+$HOME/.config/yabai/scripts/signal_uebersicht.sh
+sleep 3
 $HOME/.config/yabai/scripts/ensure_spaces.sh
 $HOME/.config/yabai/scripts/signal_uebersicht.sh
--- yabai/scripts/signal_uebersicht.sh
+++ yabai/scripts/signal_uebersicht.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
+# NOTE: If uebersicht details change and the websocket approach breaks, just uncomment
+# the following line to fall back to applescript.
+# compatibility_mode="true"
+
 
 # This is a slight performance optimization to get a faster refresh, as the Applescript interface
 # is pretty slow.
 # If this ever breaks we can always fall back to using applescript script to regain functionality.
-
-# Uncomment to use applescript
-# osascript -e 'tell application id "tracesOf.Uebersicht" to refresh widget id "nibar-yabai-jsx"'
-
-if [[ $(type -P websocat) = "" ]]; then
+if [[ $(type -P websocat) = "" || "$compatibility_mode" = "true" ]]; then
    echo "Falling back to applescript, websocat not installed. https://github.com/vi/websocat"
    osascript -e 'tell application id "tracesOf.Uebersicht" to refresh widget id "nibar-yabai-jsx"'
-else
-    echo "Signaling through websocket"
-    # This is the Uebersicht server address.
-    HOST=127.0.0.1:41416
-
-    # The server checks the origin to filter out invalid requests, so make sure
-    # we set it.
-    echo '{"type":"WIDGET_WANTS_REFRESH","payload":"nibar-yabai-jsx"}' |
-        websocat -E --origin http://$HOST ws://$HOST
+   return
 fi
+
+# This is the Uebersicht server address.
+HOST=127.0.0.1:41416
+
+payload="\"nibar-yabai-jsx\""
+ws_event="WIDGET_WANTS_REFRESH"
+echo "Posting event ${ws_event} with payload ${payload}"
+# The server checks the origin to filter out invalid requests, so make sure we set it.
+echo "{\"type\":\"${ws_event}\",\"payload\":${payload}}" |
+    websocat -E --origin http://$HOST ws://$HOST
