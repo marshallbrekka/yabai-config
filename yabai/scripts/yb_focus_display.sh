7cd012aa43d33b13caf09a6bb53ae311a35f94db
Marshall Brekka <marshallsmedia@gmail.com>
2020-05-25 18:40:59 -0700

    Improve multi-monitor handling both for focusing and for moving windows between them

--- yabai/scripts/yb_focus_display.sh
+++ yabai/scripts/yb_focus_display.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e
 
 echo "Focusing display $1"
-yabai -m display --focus $1
+# Yabai display numbers are not ordered by their arrangement.
+# for my purposes I want the numbering to always increase from left->right.
+
+# jq -e will exit non-zero if we get a null result back
+DISPLAY_INDEX=$(yabai -m query --displays | jq -e "sort_by(.frame.x) | .[$1].index")
+
+echo "Found display index $DISPLAY_INDEX"
+yabai -m display --focus $DISPLAY_INDEX
 ~/.config/yabai/scripts/signal_uebersicht.sh
--- yabai/scripts/yb_move_window_to_space.sh
+++ yabai/scripts/yb_move_window_to_space.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
 set -e;
 
-display=$(yabai -m query --displays --display | jq -c '.index')
+display_arg=""
+space_arg=""
+focus_arg=0
 
-echo "moving window to space $1 on display $display"
-yabai -m window --space "display-${display}_space-${1}"
+while [ "$1" != "" ]; do
+    case $1 in
+        -d | --display ) shift
+                         display_arg=$1
+                         ;;
+        -s | --space )   shift
+			 space_arg=$1
+			 ;;
+        -f | --focus )   focus_arg=1
+    esac
+    shift
+done
+
+display_index=""
+space_label=""
+
+# If the caller specified the desired display as an argument
+# then lookup its index based on the ordering from left->right.
+# otherwise assume the current display
+if [[ "$display_arg" != "" ]]; then
+    echo "Looking up display by $display_arg"
+    display_index=$(yabai -m query --displays | jq -e "sort_by(.frame.x) | .[$display_arg].index")
+else
+    display_index=$(yabai -m query --displays --display | jq -c '.index')
+fi
+echo "found display index $display_index"
+
+if [[ "$space_arg" != "" ]]; then
+    space_label="display-${display_index}_space-${space_arg}"
+else
+    space_label=$(yabai -m query --spaces --display $display_index | jq -e -r '.[] | select(.visible == 1) | .label')
+fi
+
+echo "moving window to space with label $space_label"
+window_id=$(yabai -m query --windows --window | jq '.id')
+yabai -m window --space $space_label
+
+if [[ $focus_arg == 1 ]]; then
+    echo "Focusing window $window_id"
+    yabai -m window --focus $window_id
+fi
